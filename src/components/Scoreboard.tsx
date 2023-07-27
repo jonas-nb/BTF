0978ee458a6f0305f7411f9d526cff4f04afb9ba
Jonas Batista <jonas.nb4@gmail.com>
2023-07-27 19:41:08 -0300

    Fix score mode and add modal for show winner!

--- src/components/Scoreboard.tsx
+++ src/components/Scoreboard.tsx
@@ -1,77 +1,108 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./style.css";
-
-interface Team {
-  name: string;
-  score: number;
-  sets: number;
-}
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 
 const Scoreboard: React.FC = () => {
-  const [teamA, setTeamA] = useState<Team>({
-    name: "Time A",
+  const initialTeamState = {
+    name: "Team",
     score: 0,
-    sets: 0,
-  });
+    game: 0,
+  };
 
-  const [teamB, setTeamB] = useState<Team>({
-    name: "Time B",
-    score: 0,
-    sets: 0,
-  });
+  localStorage.clear();
 
-  const incrementScore = (team: Team) => {
-    if (team.score < 40) {
-      setPoints(team, 15);
-    } else if (team.score === 40) {
-      setPoints(team, 0);
-      setSets(team);
+  const [teamA, setTeamA] = useState({ ...initialTeamState, name: "Team A" });
+  const [teamB, setTeamB] = useState({ ...initialTeamState, name: "Team B" });
+  const [modal, setModal] = useState(false);
+  const [alertWin, setAlertWin] = useState("");
+  const toggle = () => setModal(!modal);
+
+  // Estado para controlar se a lista de atualizações deve ser mostrada
+  const [showUpdates, setShowUpdates] = useState(false);
+
+  // Efeito para verificar se a lista de atualizações deve ser mostrada
+  useEffect(() => {
+    // Verifica se a flag "updatesShown" está presente no localStorage
+    const updatesShown = localStorage.getItem("updatesShown");
+
+    // Se a flag não estiver presente, mostra as atualizações
+    if (!updatesShown) {
+      setShowUpdates(true);
+    }
+  }, []);
+
+  // Função para fechar o modal e definir a flag no localStorage para não mostrar novamente
+  const closeModal = () => {
+    setShowUpdates(false);
+    localStorage.setItem("updatesShown", "true");
+  };
+
+  const handleGameClick = (team) => {
+    if (team.name === "Team A") {
+      if (teamA.game === 6) {
+        setTeamA({ ...teamA, game: 0 });
+        setShowWinner(true);
+      } else {
+        setTeamA({ ...teamA, game: teamA.game + 1 });
+      }
+    } else {
+      if (teamB.game === 6) {
+        setTeamB({ ...teamB, game: 0 });
+        setShowWinner(true);
+      } else {
+        setTeamB({ ...teamB, game: teamB.game + 1 });
+      }
     }
   };
 
-  const decrementScore = (team: Team) => {
-    if (team.score > 0) {
-      setPoints(team, -15);
-    }
-  };
-
-  const resetSets = () => {
-    resetPoints();
-    setTeamA({ ...teamA, sets: 0 });
-    setTeamB({ ...teamB, sets: 0 });
-  };
-
-  const setPoints = (team: Team, points: number) => {
-    team.score += points;
-    if (team.score === 45) {
+  const incrementScore = (team) => {
+    if (team.score === 0) {
+      team.score = 15;
+    } else if (team.score === 15) {
+      team.score = 30;
+    } else if (team.score === 30) {
       team.score = 40;
+    } else if (team.score === 40) {
+      // If a team reaches 40 points and wins the game
+      if (team.game === 6) {
+        // Game over, team won
+        setModal(!modal);
+        setAlertWin(`${team.name} Ganhou 🎉`);
+        console.log(`${team.name} won the game!`);
+        // Reset the score for both teams
+        setTeamA({ ...initialTeamState, name: "Team A" });
+        setTeamB({ ...initialTeamState, name: "Team B" });
+        return;
+      }
+      team.score = 0;
+      team.game++;
     }
+
     updateTeam(team);
   };
 
-  const setSets = (team: Team) => {
-    team.sets += 1;
-    if (team.sets === 3) {
-      alert(`${team.name} venceu o jogo!`);
-      resetScores();
-    } else {
-      alert(`Set vencido por ${team.name}!`);
-      resetPoints();
+  const decrementScore = (team) => {
+    if (team.score === 0) {
+      return; // No negative points
+    } else if (team.score === 15) {
+      team.score = 0;
+    } else if (team.score === 30) {
+      team.score = 15;
+    } else if (team.score === 40) {
+      team.score = 30;
     }
+
+    updateTeam(team);
   };
 
   const resetPoints = () => {
-    setTeamA({ ...teamA, score: 0 });
-    setTeamB({ ...teamB, score: 0 });
+    setTeamA({ ...initialTeamState, name: "Team A" });
+    setTeamB({ ...initialTeamState, name: "Team B" });
   };
 
-  const resetScores = () => {
-    setTeamA({ ...teamA, sets: 0 });
-    setTeamB({ ...teamB, sets: 0 });
-  };
-
-  const updateTeam = (team: Team) => {
-    if (team.name === "Time A") {
+  const updateTeam = (team) => {
+    if (team.name === "Team A") {
       setTeamA({ ...team });
     } else {
       setTeamB({ ...team });
@@ -79,27 +110,32 @@ const Scoreboard: React.FC = () => {
   };
 
   return (
-    <div className="scoreboard ">
+    <div className="scoreboard">
       <div className="team-container w-full h-screen sm:flex-row  flex-col md:flex-col">
         <div className="w-full h-full sm:h-screen  bg-black text-white flex flex-col items-center justify-around">
           <div className="team-name text-3xl sm:self-end md:self-center sm:pr-5 md:pr-0">
             {teamA.name}
           </div>
           <div className="team-score">{teamA.score}</div>
-          <div className="team-sets">Sets: {teamA.sets}</div>
+          <div
+            className="team-game"
+            onClick={() => handleGameClick(teamA)}
+            style={{ cursor: "pointer" }}
+          >
+            game: {teamA.game}
+          </div>
           <div className="btn-center">
-            <button
-              className="bg-white/10 text-3xl"
-              onClick={() => incrementScore(teamA)}
-            >
-              +1
-            </button>
-
             <button
               className="bg-white/10 text-3xl"
               onClick={() => decrementScore(teamA)}
             >
               -1
+            </button>
+            <button
+              className="bg-white/10 text-3xl"
+              onClick={() => incrementScore(teamA)}
+            >
+              +1
             </button>
           </div>
         </div>
@@ -108,22 +144,52 @@ const Scoreboard: React.FC = () => {
             {teamB.name}
           </div>
           <div className="team-score">{teamB.score}</div>
-          <div className="team-sets">Sets: {teamB.sets}</div>
+          <div
+            className="team-game"
+            onClick={() => handleGameClick(teamB)}
+            style={{ cursor: "pointer" }}
+          >
+            game: {teamB.game}
+          </div>
           <div className="btn-center">
-            <button
-              className="bg-white/40 text-3xl"
-              onClick={() => incrementScore(teamB)}
-            >
-              +1
-            </button>
-
             <button
               className="bg-white/40 text-3xl"
               onClick={() => decrementScore(teamB)}
             >
               -1
             </button>
+            <button
+              className="bg-white/40 text-3xl"
+              onClick={() => incrementScore(teamB)}
+            >
+              +1
+            </button>
           </div>
+
+          <Modal isOpen={modal}>
+            <ModalBody className="text-6xl text-center">{alertWin}</ModalBody>
+            <ModalFooter>
+              <Button className="bg-black" onClick={toggle}>
+                Jogar novamente
+              </Button>
+            </ModalFooter>
+          </Modal>
+          {showUpdates && (
+            <Modal>
+              <ModalBody>
+                {/* Conteúdo das atualizações */}
+                <h2>Atualizações</h2>
+                <ul>
+                  <li>Foi feito a atualização na contagem de pontos</li>
+                  <li>
+                    Ao clicar no campo game adciona mais um caso necessário
+                  </li>
+                </ul>
+                <p>Create with 💚 by Jonas Batista</p>
+                <button onClick={closeModal}>Fechar</button>
+              </ModalBody>
+            </Modal>
+          )}
         </div>
       </div>
 
@@ -133,12 +199,6 @@ const Scoreboard: React.FC = () => {
           onClick={resetPoints}
         >
           Resetar Pontuação
-        </button>
-        <button
-          className="reset-sets-button w-[8rem] bg-white/10 sm:bg-black/20"
-          onClick={resetSets}
-        >
-          Resetar Sets
         </button>
       </div>
     </div>
