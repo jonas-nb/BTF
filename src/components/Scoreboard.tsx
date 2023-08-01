8525a61090ea280c2c70a995ea9d068ac5fc069e
Jonas Batista <jonas.nb4@gmail.com>
2023-08-01 14:14:29 -0300

    atualização para diferentes tipo de tela

--- src/components/Scoreboard.tsx
+++ src/components/Scoreboard.tsx
@@ -98,141 +98,117 @@ const Scoreboard: React.FC = () => {
     }
   };
 
-  const [isLandscape, setIsLandscape] = useState(
-    window.innerWidth > window.innerHeight
-  );
-
-  useEffect(() => {
-    const handleOrientationChange = () => {
-      setIsLandscape(window.innerWidth > window.innerHeight);
-    };
-
-    window.addEventListener("resize", handleOrientationChange);
-
-    return () => {
-      window.removeEventListener("resize", handleOrientationChange);
-    };
-  }, []);
-
   return (
     <div>
       <div>
-        {isLandscape ? (
-          // Conteúdo da página em modo paisagem
-          <div>
-            <div className="scoreboard bg-container">
-              <div className="flex">
-                {/* section team A */}
-                <div className="scoreboard__container border-r border-[#001a064d]">
-                  <div className="team-name bg-[#1df500] text-black">
-                    {teamA.name}
-                  </div>
-
-                  {/* container dos pontos */}
-                  <div className="scoreborad__score">
-                    <button
-                      className="p-2 w-14 h-12 bg-white/40 text-xl hover:border-none active:bg-[#1ef554] shadow-black shadow-sm"
-                      onClick={() => decrementScore(teamA)}
-                    >
-                      -
-                    </button>
-                    <div className="score">{teamA.score}</div>
-                    <button
-                      className="p-2 w-14 h-12 bg-white/40 text-xl hover:border-none active:bg-[#1ef554] shadow-black shadow-sm"
-                      onClick={() => incrementScore(teamA)}
-                    >
-                      +
-                    </button>
-                  </div>
-
-                  {/* controle do game */}
-                  <div className="scoreboard__game">
-                    <div className="title">game</div>
-                    <div
-                      className="btn"
-                      onClick={() => handleGameClick(teamA)}
-                      style={{ cursor: "pointer" }}
-                    >
-                      {teamA.game}
-                    </div>
-                  </div>
+        <div>
+          <div className="scoreboard bg-container">
+            <div className="flex scoreboard__col">
+              {/* section team A */}
+              <div className="scoreboard__container border-r border-[#001a064d]">
+                <div className="team-name bg-[#1df500] text-black">
+                  {teamA.name}
                 </div>
 
-                {/* section team B */}
-                <div className="scoreboard__container border-l border-[#001a064d]">
-                  <div className="team-name bg-black text-white">
-                    {teamB.name}
-                  </div>
-
-                  {/* container dos pontos */}
-                  <div className="scoreborad__score ">
-                    <button
-                      className="p-2 w-14 h-12 bg-white/40 text-xl hover:border-none active:bg-[#1ef554] shadow-black shadow-sm"
-                      onClick={() => decrementScore(teamB)}
-                    >
-                      -
-                    </button>
-                    <div className="score">{teamB.score}</div>
-                    <button
-                      className="p-2 w-14 h-12 bg-white/40 text-xl hover:border-none active:bg-[#1ef554] shadow-black shadow-sm"
-                      onClick={() => incrementScore(teamB)}
-                    >
-                      +
-                    </button>
-                  </div>
-
-                  {/* controle do game */}
-                  <div className="scoreboard__game">
-                    <div className="title">game</div>
-                    <div
-                      className="btn"
-                      onClick={() => handleGameClick(teamB)}
-                      style={{ cursor: "pointer" }}
-                    >
-                      {teamB.game}
-                    </div>
-                  </div>
+                {/* container dos pontos */}
+                <div className="scoreboard__score">
+                  <button
+                    className="p-2 w-14 h-12 bg-white/40 text-xl hover:border-none active:bg-[#1ef554] shadow-black shadow-sm"
+                    onClick={() => decrementScore(teamA)}
+                  >
+                    -
+                  </button>
+                  <div className="score">{teamA.score}</div>
+                  <button
+                    className="p-2 w-14 h-12 bg-white/40 text-xl hover:border-none active:bg-[#1ef554] shadow-black shadow-sm"
+                    onClick={() => incrementScore(teamA)}
+                  >
+                    +
+                  </button>
                 </div>
-                {/* modal */}
-                <div>
-                  <Modal className="bg" isOpen={modal} toggle={toggle}>
-                    <ModalHeader className="text-4xl m-auto" toggle={toggle}>
-                      {alertWin}
-                    </ModalHeader>
-                    <Button
-                      onClick={toggle}
-                      className="bg-black w-36 m-auto mb-5"
-                    >
-                      Jogar Novamente
-                    </Button>
-                  </Modal>
+
+                {/* controle do game */}
+                <div className="scoreboard__game">
+                  <div className="title">game</div>
+                  <div
+                    className="btn"
+                    onClick={() => handleGameClick(teamA)}
+                    style={{ cursor: "pointer" }}
+                  >
+                    {teamA.game}
+                  </div>
                 </div>
               </div>
 
-              <div className="fixed bottom-0 left-0 w-[5rem] h-16">
-                <button
-                  className="rounded-none rounded-r-md flex items-center justify-center w-[5rem]  bg-white/30 hover:bg-green-500 hover:border-none"
-                  onClick={resetPoints}
-                >
-                  Resetar Pontos
-                </button>
-                <div className="fixed bottom-0 right-0 w-[5rem] h-16">
+              {/* section team B */}
+              <div className="scoreboard__container border-l border-[#001a064d]">
+                <div className="team-name bg-black text-white">
+                  {teamB.name}
+                </div>
+
+                {/* container dos pontos */}
+                <div className="scoreboard__score ">
                   <button
-                    className="rounded-none rounded-l-md flex items-center justify-center w-[5rem]  bg-white/30 hover:bg-green-500 hover:border-none"
-                    onClick={resetPoints}
+                    className="p-2 w-14 h-12 bg-white/40 text-xl hover:border-none active:bg-[#1ef554] shadow-black shadow-sm"
+                    onClick={() => decrementScore(teamB)}
                   >
-                    Modo Jogo
+                    -
+                  </button>
+                  <div className="score">{teamB.score}</div>
+                  <button
+                    className="p-2 w-14 h-12 bg-white/40 text-xl hover:border-none active:bg-[#1ef554] shadow-black shadow-sm"
+                    onClick={() => incrementScore(teamB)}
+                  >
+                    +
                   </button>
                 </div>
+
+                {/* controle do game */}
+                <div className="scoreboard__game">
+                  <div className="title">game</div>
+                  <div
+                    className="btn"
+                    onClick={() => handleGameClick(teamB)}
+                    style={{ cursor: "pointer" }}
+                  >
+                    {teamB.game}
+                  </div>
+                </div>
+              </div>
+              {/* modal */}
+              <div>
+                <Modal className="bg" isOpen={modal} toggle={toggle}>
+                  <ModalHeader className="text-4xl m-auto" toggle={toggle}>
+                    {alertWin}
+                  </ModalHeader>
+                  <Button
+                    onClick={toggle}
+                    className="bg-black w-36 m-auto mb-5"
+                  >
+                    Jogar Novamente
+                  </Button>
+                </Modal>
+              </div>
+            </div>
+
+            <div className="fixed bottom-0 left-0 w-[5rem] h-16">
+              <button
+                className="rounded-none rounded-r-md flex items-center justify-center w-[5rem]  bg-white/30 hover:bg-green-500 hover:border-none"
+                onClick={resetPoints}
+              >
+                Resetar Pontos
+              </button>
+              <div className="fixed bottom-0 right-0 w-[5rem] h-16">
+                <button
+                  className="rounded-none rounded-l-md flex items-center justify-center w-[5rem]  bg-white/30 hover:bg-green-500 hover:border-none"
+                  onClick={resetPoints}
+                >
+                  Modo Jogo
+                </button>
               </div>
             </div>
           </div>
-        ) : (
-          // Mensagem para girar o dispositivo para o modo paisagem
-          <div>
-            <h1>Por favor, vire seu dispositivo para o modo paisagem.</h1>
-          </div>
-        )}
+        </div>
       </div>
     </div>
   );
